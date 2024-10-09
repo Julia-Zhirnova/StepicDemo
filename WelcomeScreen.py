721654a7b68bc0658ade07c519d15648b943f27d
kabinet303 <kabinet303@localhost.localdomain>
2024-10-09 12:59:41 +0300

    добавили комментарии

--- WelcomeScreen.py
+++ WelcomeScreen.py
@@ -1,10 +1,13 @@
 #  widget - это имя, присваиваемое компоненту пользовательского интерфейса,
 #  с которым пользователь может взаимодействовать 
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import (    
     QDialog # это базовый класс диалогового окна
 )
 
 from PyQt5.uic import loadUi # загрузка интерфейса, созданного в Qt Creator
+
+import sqlite3
 
 # Окно приветствия
 class WelcomeScreen(QDialog):
@@ -17,3 +20,25 @@ class WelcomeScreen(QDialog):
         """
         super(WelcomeScreen, self).__init__()
         loadUi("welcomescreen.ui",self) # загружаем интерфейс.
+        self.PasswordField.setEchoMode(QtWidgets.QLineEdit.Password) # скрываем пароль
+        self.SignInButton.clicked.connect(self.signupfunction) # нажати на кнопку и вызов функции
+
+    def signupfunction(self): # создаем функцию регистрации
+        user = self.LoginField.text() # создаем пользователя и получаем из поля ввода логина введенный текст
+        password = self.PasswordField.text() # создаем пароль и получаем из поля ввода пароля введенный текст
+        print(user, password) # выводит логин и пароль
+
+        if len(user)==0 or len(password)==0: # если пользователь оставил пустые поля
+            self.ErrorField.setText("Заполните все поля") # выводим ошибку в поле
+        else:
+            self.ErrorField.setText("Все ок") # выводим что все хорошо в поле
+
+        conn = sqlite3.connect("uchet.db") # подключение к базе данных в () изменить на название своей БД
+        cur = conn.cursor() # переменная для запросов
+
+        cur.execute('SELECT typeID FROM users WHERE login=(?) and password=(?)', [user, password]) # получаем тип пользователя, логин и пароль которого был введен
+        typeUser = cur.fetchone() # получает только один тип пользователя
+        print(typeUser[0]) # выводит тип пользователя без скобок                     
+
+        conn.commit() # сохраняет в подключении запросы
+        conn.close() # закрываем подключение
